321513bb657cdf2d78d120a44694b3fe8baf1ba5
girlwithglasses <gwg@Cygnus-X1.local>
2015-08-18 17:41:39 -0700

    Fixing connection params

--- IMG/Util/DBIxConnector.pm
+++ IMG/Util/DBIxConnector.pm
@@ -18,15 +18,26 @@ Create a database handle for a database using DBIx::Connector
 sub get_dbix_connector {
 	my $arg_h = shift // croak "No connection parameters specified!";
 
-	if (! ref $arg_h || ref $arg_h ne 'HASH' || ! $arg_h->{dsn} ) {
+	if (! ref $arg_h || ref $arg_h ne 'HASH' ) {
 		croak "database connection params should be specified as a hash ref and include the key 'dsn'";
+	}
+
+	if (! $arg_h->{dsn} ) {
+		if (! $arg_h->{database} || ! $arg_h->{driver} ) {
+
+			croak __PACKAGE__ . ' ' . (caller(0))[3]
+			. " requires either a DSN string or the database name and driver";
+		}
+		$arg_h->{dsn} = 'dbi'
+			. ':' . $arg_h->{driver}
+			. ':' . $arg_h->{database};
 	}
 
 	my $conn = DBIx::Connector->new(
 		$arg_h->{dsn},
 		$arg_h->{username} // $arg_h->{user} // undef,
-		$arg_h->{password} // undef,
-		$arg_h->{options} // { RaiseError => 1 }
+		$arg_h->{password} // $arg_h->{pass} // undef,
+		$arg_h->{options} // $arg_h->{dbi_params} // { RaiseError => 1 }
 	) or die "Could not create a DB connection: $DBI::errstr";
 
 	return $conn;
